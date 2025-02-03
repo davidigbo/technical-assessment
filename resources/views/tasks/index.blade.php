8b04de8796c33507c9d382d68a6097d99749b3e6
David Igbo <davidokpah396@gmail.com>
2025-02-03 10:14:58 +0100

    craete index view

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>Task List</h1>
+  <a href="{{ route('tasks.create') }}" class="btn btn-primary">Create Task</a>
+
+  <ul>
+     @foreach($tasks as $task)
+        <li>
+            <a href="{{ route('tasks.show', $task->id) }}">{{ $task->title }}</a>
+            ({{ $task->status }})
+
+            <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary">Edit</a>
+            <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="d-inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </li>
+      @endforeach
+    </ul>
+@endsection
