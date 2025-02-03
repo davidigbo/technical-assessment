3b7caf51c24d59c5fdcfff705225669527730693
David Igbo <davidokpah396@gmail.com>
2025-02-03 10:47:24 +0100

    Implemented Task Management System with authentication

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth']->group(function () {
+Route::middleware(['auth'])->group(function () {
    Route::resource('tasks', TaskController::class);
-}));
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
